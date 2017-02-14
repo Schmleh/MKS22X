5aa1077cd8ebf39d842d984b3ae90caaa7a7e33b
Schmleh <fredschmleh@gmail.com>
2017-02-14 05:50:05 -0500

    luna

---
+++ 03KnightBoard/KnightBoard.java
@@ -0,0 +1,18 @@
+public class KnightBoard{
+    public int[][] Board;
+
+    public KnightBoard(int startingRows, int startingCols){
+	Board = new int[startingRows][startingCols];
+    }
+
+    public String toString(){
+    }
+    //blank if you never called solve or when there is no solution
+
+    public void solve() {
+    }
+
+    private boolean solveH(int row ,int col, int level){
+    }
+ // level is the # of the knight
+}
