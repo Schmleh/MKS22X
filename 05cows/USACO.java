1552b0e8c9c0381397ce124fd704570a20ff2f35
Fred Ula <Fred Ula>
2017-03-12 19:45:55 -0400

    bronze and silver

--- 05cows/USACO.java
+++ 05cows/USACO.java
@@ -125,10 +125,4 @@ public class USACO{
 	    return -1;
 	}
     }
-    
-    public static void main(String[] args){
-	for(int i = 1; i < 11; i++){
-	    System.out.println(USACO.silver("ctravel." + i + ".in"));
-	}
-    }
 }
