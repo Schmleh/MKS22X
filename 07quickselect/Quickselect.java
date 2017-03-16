a60c9cf7cb7a636ce6a09c2786fc9fccf8a14b16
Schmleh <fredschmleh@gmail.com>
2017-03-16 14:50:02 -0400

    meeehhh

--- 07quickselect/Quickselect.java
+++ 07quickselect/Quickselect.java
@@ -1,6 +1,8 @@
 public class Quickselect{
 
-    public static int partition(int[] nums, int start, int end, int pos){
+    public static int partition(int[] nums, int start, int end){
+	int pos = (int)(start + (Math.random() * (end - start)));
+	//	System.out.println("Random Index: " + pos);
 	int[] two = new int[nums.length];
 	for(int i = 0; i < start; i ++){
 	    two[i] = nums[i];
@@ -27,37 +29,35 @@ public class Quickselect{
 	for (int j = 0; j < nums.length; j ++){
 	    nums[j] = two[j];
 	}
-	for ( int j: nums){
-	    System.out.print(j + " ");
-	}
-	System.out.print("\n");
-	return s;
+	 for ( int j: nums){
+	     System.out.print(j + " ");
+	 }
+	 System.out.print("\n");
+	 System.out.println("Final Index: " + s);
+	 return s;
     }
     
     public static int quickFind(int[] nums, int pos){
      	int start = 0;
      	int end = nums.length;
      	int found = -100;
-     	while( found != pos - 1){
-     	    int ran = (int)(start + (Math.random() * (end - start)));
-	    System.out.println((int)(start + (Math.random() * (end - start))));
-	    System.out.println(start);
-	    System.out.println(end);
-	    System.out.println(pos);
-	    System.out.println(ran);
-     	    found = partition(nums, start, end, ran);
-     	    if (found > pos - 1){
-     		end = found;
+     	while( found != pos){
+	    System.out.println("Start: " + start);
+	    System.out.println("End: " + end);
+     	    found = partition(nums, start, end);
+     	    if (found > pos){
+     		end = found - 1;
      	    }
-     	    if (found < pos - 1){
-     		start = found;
+     	    if (found < pos){
+     		start = found + 1;
      	    }
+	    System.out.println("");
     	}
     	return nums[found];
     }
 
     public static void main(String[] args){
-     	int[] num = {999,999,999,4,1,0,3,2,999,999,999};
-     	System.out.println(quickFind(num, 7));
+     	int[] num = {999,999,999,4,1,0,3,2,999,999,999, 42, 32, 91, 912, -3, 8912, -23, 235, -23489, 923};
+     	System.out.println(quickFind(num, 4));
      }
 }
