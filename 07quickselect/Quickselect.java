42e5c4addb8ff710543a5bc3825319aca56017cb
Fred Ula <Fred Ula>
2017-03-17 14:47:00 -0400

    Actually works???

--- 07quickselect/Quickselect.java
+++ 07quickselect/Quickselect.java
@@ -46,7 +46,7 @@ public class Quickselect{
 	    System.out.println("End: " + end);
      	    found = partition(nums, start, end);
      	    if (found > pos){
-     		end = found - 1;
+     		end = found;
      	    }
      	    if (found < pos){
      		start = found + 1;
@@ -57,7 +57,7 @@ public class Quickselect{
     }
 
     public static void main(String[] args){
-     	int[] num = {999,999,999,4,1,0,3,2,999,999,999, 42, 32, 91, 912, -3, 8912, -23, 235, -23489, 923};
-     	System.out.println(quickFind(num, 4));
+     	int[] num = {999,999,999,4,1,0,3,2,999,999,999, -2345, -2, -3 ,0, 8 ,91};
+     	System.out.println(quickFind(num, 9));
      }
 }
