3ee5b27f494b76066dce868b7302107742cbadfc
Schmleh <fredschmleh@gmail.com>
2017-03-21 14:49:21 -0400

    work

--- 07quickselect/Quickselect.java
+++ 07quickselect/Quickselect.java
@@ -56,8 +56,51 @@ public class Quickselect{
     	return nums[found];
     }
 
+    public static void swap(int[] ary, int one, int two){
+	int three = 0;
+	three = ary[two];
+	ary[two] = ary[one];
+	ary[one] = three;
+    }
+
+    public static void quicksort(int[] ary){
+	quickhelp(ary, 0, 0, ary.length - 1);
+    }
+
+    public static void quickhelp(int[] ary, int start, int ref, int end){
+	int pos = (int)(start + (Math.random() * (end - start)));
+	swap(ary, start, pos);
+	int s = start ++;
+	int r = ref;
+	int e = end;
+	while (s <= e){
+	    if (ary[s] == ary[start]){
+		r ++;
+	    }
+	    if (ary[s] < ary[start]){
+		swap(ary, s, r);
+		s ++;
+		r ++;
+		ref ++;
+	    }
+	    if (ary[s] > ary[start]){
+		swap(ary, s, e);
+		e --;
+	    }
+	}
+	swap(ary, start, e);
+	if( end - start > 1){
+	    quickhelp(ary, start, ref, e);
+	    quickhelp(ary, e, r ++, end);
+	}
+    }
+
     public static void main(String[] args){
      	int[] num = {999,999,999,4,1,0,3,2,999,999,999, 42, 32, 91, 912, -3, 8912, -23, 235, -23489, 923};
-     	System.out.println(quickFind(num, 4));
-     }
+     	
+	quicksort(num);
+	for(int i = 0; i < num.length; i ++){
+	    System.out.print(num[i] + " ");
+	}
+    }
 }
